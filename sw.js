607f6af323b91120fb3e15840ccf07ce3d48b6f5
Dizzylover-G2X <theunfade25@gmail.com>
2026-08-24 10:28:09 +0900

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,17 @@
+const CACHE_NAME = 'bearing-calc-v1';
+const FILES_TO_CACHE = [
+    './index.html',
+    './manifest.json'
+];
+
+self.addEventListener('install', (evt) => {
+    evt.waitUntil(
+        caches.open(CACHE_NAME).then((cache) => cache.addAll(FILES_TO_CACHE))
+    );
+});
+
+self.addEventListener('fetch', (evt) => {
+    evt.respondWith(
+        caches.match(evt.request).then((response) => response || fetch(evt.request))
+    );
+});
