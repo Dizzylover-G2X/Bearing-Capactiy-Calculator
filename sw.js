4711651d63fc8694c70c9b47bfc4ef97f8c820c5
Dizzylover-G2X <theunfade25@gmail.com>
2026-08-24 10:52:20 +0900

    Update sw.js
    index.html 항목 삭제 - 홈화면 불러오기 수정

--- sw.js
+++ sw.js
@@ -1,9 +1,11 @@
-const CACHE_NAME = 'bearing-calc-v1';
+const CACHE_NAME = 'bearing-calc-v2'; // v1을 v2로 살짝 바꿔주면 폰에서 업데이트를 더 빨리 인식합니다.
 const FILES_TO_CACHE = [
+    './',
     './index.html',
     './manifest.json'
 ];
 
+// 아래 코드는 기존과 동일하게 유지
 self.addEventListener('install', (evt) => {
     evt.waitUntil(
         caches.open(CACHE_NAME).then((cache) => cache.addAll(FILES_TO_CACHE))
